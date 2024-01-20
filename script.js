893bc0dac27466bd81eecd451794e58caf70b546
Jay Rana <138268605+ranajay531@users.noreply.github.com>
2024-01-20 16:54:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+
+/* Hamburger Menu */
+function toggleMenu() {
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
+}
+
+/* Tabbed Content */
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function toggleTab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+/* Scroll Reveal */
+ScrollReveal({
+  reset: true,
+  duration: 1000,
+  distance: '80px',
+  delay: 100
+});
+
+ScrollReveal().reveal('.section_text, .contact-text', { origin: 'left' });
+ScrollReveal().reveal('.about-text, .tab-links, .tab-contents,', { origin: 'right' });
+ScrollReveal().reveal('.about-subtitle, .about-title, .contact-subtitle, .contact-title', { origin: 'top' });
+ScrollReveal().reveal('.about-col-1, .swiper', { origin: 'bottom' });
+
+/* Contact Form */
+
+/* Swiper */
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  }
+});
