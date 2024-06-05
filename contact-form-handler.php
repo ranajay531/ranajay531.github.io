2c4d90ce5f7a7b4ef7ea015810e6f5000bed3499
Jay Rana <ranajay531@gmail.com>
2024-06-05 17:09:14 -0400

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,13 +1,16 @@
 <?php
 
-    $name = $_POST['name'];
-    $user_email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = $_POST['Name'];
+    $user_email = $_POST['Email'];
+    $subject = $_POST['Subject'];
+    $message = $_POST['Message'];
+    $phone = $_POST['Phone'];
 
     $email_from = "ranajay531@gmail.com";
-    $email_subject = "New Form Submission";
+    $email_subject = "New Form Submission: $subject";
     $email_body = "User Name: $name.\n".
                     "User Email: $user_email.\n".
+                    "User Phone: $phone.\n".
                     "User Message: $message.\n";
     
     $to = "ranajay531@gmail.com";
